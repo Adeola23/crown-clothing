7d38232abac40425f9606db29fedd665f3d7951d
Adeola23 <amosadeola92@gmail.com>
2021-10-15 14:39:21 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/component/Preview collection/preview.js
+++ src/component/Preview collection/preview.js
@@ -1,18 +1,23 @@
 import React from "react";
 import './preview.styles.scss'
 import CollectionItem from "../Collection item/collection-item";
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionPreview = ({title,items}) => (
     <div className={'collection-preview'}>
         <h1 className={'title'}>{title.toUpperCase()}</h1>
         <div className={'preview'}>
             {
-                items.filter((item, idx)=> idx<4).map(({id, ...otherCollectionItems}) =>
-                    <CollectionItem key={'id'} {...otherCollectionItems}/>
+                items.filter((item, idx)=> idx<4).map(item =>
+                    <CollectionItem key={item.id} item={item}/>
                 )
             }
 
+
+
         </div>
+
+
     </div>
 )
 
